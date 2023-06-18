d8689c170d143e96a08b300fcb177f747138df24
N7301 <Natatime@163.com>
2023-06-18 15:28:44 +0800

    :sparkles: Fetcher interface--BrowserFetch
    add self-defined UserAgent to get douban

--- collect/collect.go
+++ collect/collect.go
@@ -7,7 +7,7 @@ import (
 	"golang.org/x/text/encoding"
 	"golang.org/x/text/encoding/unicode"
 	"golang.org/x/text/transform"
-	"io/ioutil"
+	"io"
 	"net/http"
 )
 
@@ -35,8 +35,32 @@ func (BaseFetch) Get(url string) ([]byte, error) {
 	bodyReader := bufio.NewReader(resp.Body)
 	e := DeterminEncoding(bodyReader)
 	utf8Reader := transform.NewReader(bodyReader, e.NewDecoder())
-	return ioutil.ReadAll(utf8Reader)
+	return io.ReadAll(utf8Reader)
+}
 
+// 模拟浏览器访问
+type BrowserFetch struct {
+}
+
+func (BrowserFetch) Get(url string) ([]byte, error) {
+	client := &http.Client{}
+
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		return nil, fmt.Errorf("get url failed:%v", err)
+	}
+
+	req.Header.Set("User-Agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.149 Safari/537.36\") resp, err := client.Do(req) if err != nil { return nil, err } bodyReader := bufio.NewReader(resp.Body) e := DeterminEncoding(bodyReader) utf8Reader := transform.NewReader(bodyReader, e.NewDecoder()) return ioutil.ReadAll(utf8Reader)}")
+
+	resp, err := client.Do(req)
+	if err != nil {
+		return nil, err
+	}
+
+	bodyReader := bufio.NewReader(resp.Body)
+	e := DeterminEncoding(bodyReader)
+	utf8Reader := transform.NewReader(bodyReader, e.NewDecoder())
+	return io.ReadAll(utf8Reader)
 }
 
 func DeterminEncoding(r *bufio.Reader) encoding.Encoding {
--- main.go
+++ main.go
@@ -1,16 +1,14 @@
 package main
 
 import (
-	"bytes"
 	"fmt"
 	"github.com/Nrich-sunny/crawler/collect"
-	"github.com/PuerkitoBio/goquery"
 )
 
 func main() {
-	url := "https://www.thepaper.cn/"
+	url := "https://book.douban.com/subject/1007305/"
 
-	var fetcher collect.Fetcher = collect.BaseFetch{}
+	var fetcher collect.Fetcher = collect.BrowserFetch{}
 	body, err := fetcher.Get(url)
 
 	if err != nil {
@@ -18,15 +16,17 @@ func main() {
 		return
 	}
 
-	// 加载 HTML 文档
-	doc, err := goquery.NewDocumentFromReader(bytes.NewReader(body))
-	if err != nil {
-		fmt.Printf("read content failed:%v\n", err)
-	}
+	fmt.Println(string(body))
 
-	doc.Find("div.index_linecard__wJq_3 a[target=_blank] h2").Each(func(i int, s *goquery.Selection) {
-		//  获取匹配标签中的文本
-		title := s.Text()
-		fmt.Printf("Review %d: %s\n", i, title)
-	})
+	//// 加载 HTML 文档
+	//doc, err := goquery.NewDocumentFromReader(bytes.NewReader(body))
+	//if err != nil {
+	//	fmt.Printf("read content failed:%v\n", err)
+	//}
+	//
+	//doc.Find("div.index_linecard__wJq_3 a[target=_blank] h2").Each(func(i int, s *goquery.Selection) {
+	//	//  获取匹配标签中的文本
+	//	title := s.Text()
+	//	fmt.Printf("Review %d: %s\n", i, title)
+	//})
 }
