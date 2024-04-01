76e37b8332a67b182c821abe2c3c889ca48178cf
N7301 <Natatime@163.com>
2024-04-01 11:25:45 +0800

    :sparkles: grpc server(etcd as reg center)
    为 Worker 服务构建了 GRPC 服务器和 HTTP 服务器,使用 go-micro 的插件将服务注册到了 etcd 注册中心当中
    
    实现存储引擎（此处还未尝试开启数据库连接）

--- go.mod
+++ go.mod
@@ -3,11 +3,17 @@ module github.com/Nrich-sunny/crawler
 go 1.16
 
 require (
+	github.com/go-micro/plugins/v4/registry/etcd v1.2.0
+	github.com/go-micro/plugins/v4/server/grpc v1.2.0
+	github.com/grpc-ecosystem/grpc-gateway/v2 v2.16.0
 	github.com/robertkrimen/otto v0.3.0
-	github.com/stretchr/testify v1.8.1
+	github.com/stretchr/testify v1.8.4
+	go-micro.dev/v4 v4.10.2
 	go.uber.org/zap v1.24.0
-	golang.org/x/net v0.11.0
-	golang.org/x/text v0.10.0
+	golang.org/x/net v0.20.0
+	golang.org/x/text v0.14.0
 	golang.org/x/time v0.5.0
+	google.golang.org/grpc v1.62.1
+	google.golang.org/protobuf v1.33.0
 	gopkg.in/natefinch/lumberjack.v2 v2.2.1
 )
--- main.go
+++ main.go
@@ -1,15 +1,25 @@
 package main
 
 import (
+	"context"
+	"fmt"
 	"github.com/Nrich-sunny/crawler/collect"
 	"github.com/Nrich-sunny/crawler/engine"
 	"github.com/Nrich-sunny/crawler/limiter"
 	"github.com/Nrich-sunny/crawler/log"
-	"github.com/Nrich-sunny/crawler/storage"
-	"github.com/Nrich-sunny/crawler/storage/sqlstorage"
+	pb "github.com/Nrich-sunny/crawler/proto/greeter"
+	"github.com/Nrich-sunny/crawler/proxy"
+	etcdReg "github.com/go-micro/plugins/v4/registry/etcd"
+	gs "github.com/go-micro/plugins/v4/server/grpc"
+	"github.com/grpc-ecosystem/grpc-gateway/v2/runtime"
+	"go-micro.dev/v4"
+	"go-micro.dev/v4/registry"
+	"go-micro.dev/v4/server"
 	"go.uber.org/zap"
 	"go.uber.org/zap/zapcore"
 	"golang.org/x/time/rate"
+	"google.golang.org/grpc"
+	"net/http"
 	"time"
 )
 
@@ -23,30 +33,33 @@ func main() {
 	zap.ReplaceGlobals(logger)
 
 	// proxy
-	//proxyURLs := []string{"http://127.0.0.1:8888"}
-	//p, err := proxy.RoundRobinProxySwitcher(proxyURLs...)
-	//if err != nil {
-	//	logger.Error("RoundRobinProxySwitcher failed.")
-	//	return
-	//}
-
-	var storage storage.Storage
-	storage, err := sqlstorage.New(
-		sqlstorage.WithSqlUrl("root:123456@tcp(127.0.0.1:3326)/crawler?charset=utf8"),
-		sqlstorage.WithLogger(logger.Named("sqlDB")),
-		sqlstorage.WithBatchCount(2),
-	)
+	proxyURLs := []string{"http://127.0.0.1:8888"}
+	p, err := proxy.RoundRobinProxySwitcher(proxyURLs...)
 	if err != nil {
-		logger.Error("create sqlstorage failed")
+		logger.Error("RoundRobinProxySwitcher failed.")
 		return
 	}
 
+	//// storage
+	//var storage storage.Storage
+	//storage, err = sqlstorage.New(
+	//	sqlstorage.WithSqlUrl("root:root@tcp(127.0.0.1:3326)/crawler?charset=utf8"),
+	//	sqlstorage.WithLogger(logger.Named("sqlDB")),
+	//	sqlstorage.WithBatchCount(2),
+	//)
+	//if err != nil {
+	//	logger.Error("create sqlstorage failed")
+	//	return
+	//}
+
+	// fetcher
 	var fetcher collect.Fetcher = &collect.BrowserFetch{
 		Timeout: 3000 * time.Millisecond,
-		//Proxy: p,
-		Logger: logger,
+		Proxy:   p,
+		Logger:  logger,
 	}
 
+	// speed limiter
 	// 令牌产生速率：2秒一个，桶容量：1
 	secondLimit := rate.NewLimiter(limiter.Per(1, 2*time.Second), 1)
 	// 令牌产生速率：1分钟20个
@@ -54,15 +67,15 @@ func main() {
 	// 多层限速器
 	multiLimiter := limiter.NewMultiLimiter(secondLimit, minuteLimit)
 
-	// douban cookies
+	// init tasks
 	var seeds = make([]*collect.Task, 0, 1000) // 在一开始就要分配好切片的容量, 否则频繁地扩容会影响程序的性能
 	seeds = append(seeds, &collect.Task{
 		Property: collect.Property{
 			Name: "douban_book_list",
 		},
 		Fetcher: fetcher,
-		Storage: storage,
-		Limit:   multiLimiter,
+		//Storage: storage,
+		Limit: multiLimiter,
 	})
 
 	crawler := engine.NewEngine(
@@ -73,5 +86,48 @@ func main() {
 		engine.WithScheduler(engine.NewSchedule()),
 	)
 
-	crawler.Run()
+	// worker start
+	go crawler.Run()
+
+	// start http proxy to GRPC
+	go handleHttp()
+
+	// start grpc server
+	reg := etcdReg.NewRegistry(
+		registry.Addrs(":2379"),
+	)
+	service := micro.NewService(
+		micro.Server(gs.NewServer(
+			server.Id("1"),
+		)),
+		micro.Address(":9090"),
+		micro.Registry(reg),
+		micro.Name("go.micro.server.worker"),
+	)
+	service.Init()
+	pb.RegisterGreeterHandler(service.Server(), new(Greeter))
+	if err := service.Run(); err != nil {
+		logger.Fatal("grpc server stop")
+	}
+}
+
+type Greeter struct{}
+
+func (g *Greeter) Hello(ctx context.Context, req *pb.Request, rsp *pb.Response) error {
+	rsp.Greeting = "Hello " + req.Name
+	return nil
+}
+
+func handleHttp() {
+	ctx := context.Background()
+	ctx, cancle := context.WithCancel(ctx)
+	defer cancle()
+
+	mux := runtime.NewServeMux()
+	opts := []grpc.DialOption{grpc.WithInsecure()}
+	err := pb.RegisterGreeterGwFromEndpoint(ctx, mux, "localhost:9090", opts)
+	if err != nil {
+		fmt.Println(err)
+	}
+	http.ListenAndServe(":8080", mux)
 }
